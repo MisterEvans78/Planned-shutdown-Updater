26bafeafa874ba0e4ac4699c689f9d132714b8dd
RisedSky <RisedSky@gmail.com>
2021-03-01 20:49:22 +0100

    Auto-Updater v1.0

---
+++ Startup/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TimeLaunchTheUpdatedFile = New System.Windows.Forms.Timer(Me.components)
+        Me.TimerUpdate = New System.Windows.Forms.Timer(Me.components)
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TimeLaunchTheUpdatedFile
+        '
+        Me.TimeLaunchTheUpdatedFile.Interval = 1500
+        '
+        'TimerUpdate
+        '
+        Me.TimerUpdate.Interval = 1500
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(1, 31)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(368, 23)
+        Me.ProgressBar1.TabIndex = 40
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(1, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(368, 25)
+        Me.Panel1.TabIndex = 38
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.InitialImage = Nothing
+        Me.PictureBox1.Location = New System.Drawing.Point(343, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(25, 25)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 39
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(147, 2)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 20)
+        Me.Label1.TabIndex = 37
+        Me.Label1.Text = "Updating"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Location = New System.Drawing.Point(2, 111)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(368, 25)
+        Me.Panel2.TabIndex = 39
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(112, 2)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(145, 20)
+        Me.Label2.TabIndex = 38
+        Me.Label2.Text = "Please be patient :)"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(372, 141)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Panel2)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Planned Shutdown Updater"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TimeLaunchTheUpdatedFile As Timer
+    Friend WithEvents TimerUpdate As Timer
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Startup/Form1.vb
@@ -0,0 +1,94 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Dim WC_File As New WebClient
+    Dim WC_String As New WebClient
+
+    Public StartupPath As String = Application.StartupPath
+    Public FileToUpdate As String
+
+    Public Download_Link As String
+    Public NewVersion As String
+    Public NewDownloadLink As String
+
+    'Fichier d'installation : planned_shutdown_setup.exe
+
+    Public Started_Number = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'WC_String.Headers.Add("user-agent", "Only a test!")
+        'WC_String.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
+        WC_String.Headers.Add("User-Agent", "Mozilla/4.0 (Compatible; Windows NT 5.1; MSIE 6.0)")
+        WC_File.Headers.Add("User-Agent", "Mozilla/4.0 (Compatible; Windows NT 5.1; MSIE 6.0)")
+
+        Download_Link = WC_String.DownloadString("https://api.github.com/repos/MisterEvans78/Planned-shutdown/releases/latest")
+        Debug.WriteLine("tag_name"":""(\d.\d.\d)"",")
+        'Debug.WriteLine(Download_Link)
+        Debug.WriteLine("Version : " & Regex.Match(Download_Link, "tag_name"":""(\d.\d.\d)"",").Groups.Item(1).ToString)
+        NewVersion = Regex.Match(Download_Link, "tag_name"":""(.*)"",""target_").Groups.Item(1).ToString
+        NewDownloadLink = Regex.Match(Download_Link, "browser_download_url"":""(.*)""}],").Groups.Item(1).ToString
+
+        Debug.WriteLine("azeazeazazeazezaeazezae")
+        Debug.WriteLine("----")
+        'MsgBox(Download_Link)
+        Debug.WriteLine(NewVersion)
+        Debug.WriteLine(NewDownloadLink)
+
+        FileToUpdate = $"{Path.GetTempPath}\Planned.shutdown.{NewVersion}.exe"
+
+        'MsgBox(NewVersion)
+        Continue_Loading()
+    End Sub
+
+    Private Sub Continue_Loading()
+        Try
+            If NewDownloadLink = "/" Then
+                Dim result = MessageBox.Show("It seems that the update file is not available, please contact the support to fix this problem.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                If result = DialogResult.OK Then
+                    Close()
+                Else
+                    Close()
+                End If
+            Else
+                If File.Exists(FileToUpdate) Then
+                    File.Delete(FileToUpdate)
+                End If
+                TimerUpdate.Start()
+            End If
+        Catch ex As Exception
+            MsgBox($"Try to launch the updater with admin rights{vbNewLine}Error:{ex.Message}", MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Private Sub WC_DownloadProgressChanged(sender As Object, e As DownloadProgressChangedEventArgs)
+        ProgressBar1.Value = e.ProgressPercentage
+
+        If e.ProgressPercentage = 100 Then
+            'This verification will prevent double launch
+            If Started_Number = 1 Then
+                TimeLaunchTheUpdatedFile.Start()
+            Else
+                Started_Number = 1
+            End If
+
+        End If
+    End Sub
+    Private Sub TimerUpdate_Tick(sender As Object, e As EventArgs) Handles TimerUpdate.Tick
+        TimerUpdate.Stop()
+        'MsgBox(Download_Link)
+        AddHandler WC_File.DownloadProgressChanged, AddressOf WC_DownloadProgressChanged
+        WC_File.DownloadFileAsync(New Uri(NewDownloadLink), FileToUpdate)
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Close()
+    End Sub
+
+    Private Sub TimeLaunchTheUpdatedFile_Tick(sender As Object, e As EventArgs) Handles TimeLaunchTheUpdatedFile.Tick
+        TimeLaunchTheUpdatedFile.Stop()
+        Process.Start(FileToUpdate)
+        Close()
+    End Sub
+End Class
